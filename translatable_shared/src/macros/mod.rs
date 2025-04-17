d6a384ed551a9f0d84256cadbbe7aabaecc9adbe
stifskere <esteve@memw.es>
2025-04-18 01:43:12 +0200

    docs: translatable_shared/src/macros/mod.rs

--- translatable_shared/src/macros/mod.rs
+++ translatable_shared/src/macros/mod.rs
@@ -1,2 +1,13 @@
+//! Macro helpers module.
+//!
+//! This module contains sub-modules
+//! which or either help converting
+//! compile-time structures into their
+//! runtime representations with [`TokenStream2`]
+//! or any other utils to generate
+//! runtime code.
+//!
+//! [`TokenStream2`]: proc_macro2::TokenStream
+
 pub mod collections;
 pub mod errors;
