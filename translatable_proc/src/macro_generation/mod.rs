c896554b57d318dcc9b868e982e539b91c31fe67
stifskere <esteve@memw.es>
2025-04-17 03:16:03 +0200

    docs: translatable_proc/src/macro_generation/mod.rs

--- translatable_proc/src/macro_generation/mod.rs
+++ translatable_proc/src/macro_generation/mod.rs
@@ -1,1 +1,9 @@
+//! Macro generation module.
+//!
+//! This module contains the sub-modules
+//! to generate any kind of macro, in the
+//! `lib.rs` file, a call to any of this
+//! modules may be issued with intrinsics
+//! from the `macro_input` module.
+
 pub mod translation;
