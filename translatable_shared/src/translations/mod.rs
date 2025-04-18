f912476d7e12ce553eae557874d5887cc865798a
stifskere <esteve@memw.es>
2025-04-19 00:28:31 +0200

    docs: translatable_shared/src/translations/mod.rs

--- translatable_shared/src/translations/mod.rs
+++ translatable_shared/src/translations/mod.rs
@@ -1,2 +1,10 @@
+//! Translation structures module.
+//!
+//! This module's sub-modules declare
+//! structures to manage deserialized
+//! structures from the translation files.
+//! These permit searching paths in a more
+//! rust-friendly way.
+
 pub mod collection;
 pub mod node;
