3fb24d393d1fb2d38e16e4335d9a264be67d3b0c
stifskere <esteve@memw.es>
2025-04-19 01:08:03 +0200

    docs: translatable_shared/src/lib.rs

--- translatable_shared/src/lib.rs
+++ translatable_shared/src/lib.rs
@@ -1,3 +1,13 @@
+//! Shared util declarations for `translatable` and `translatable_proc`
+//!
+//! This crate shouldn't be used by itself,
+//! since it contains macro generation code which
+//! relies on references from the `translatable` library.
+//!
+//! The `translatable` library re-exports the utils
+//! declared in this crate and exposes the necessary
+//! ones.
+
 pub mod macros;
 pub mod misc;
 pub mod translations;
