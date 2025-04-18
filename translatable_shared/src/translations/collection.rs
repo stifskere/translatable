591326173400f2e448d219cae21c317ca73a650c
stifskere <esteve@memw.es>
2025-04-19 00:25:50 +0200

    docs: translatable_shared/src/translations/collection.rs

--- translatable_shared/src/translations/collection.rs
+++ translatable_shared/src/translations/collection.rs
@@ -1,3 +1,9 @@
+//! Translation file collection module.
+//!
+//! This module declares [`TranslationNodeCollection`]
+//! a representation of each file found in the translations
+//! folder defined in the configuration file.
+
 use std::collections::HashMap;
 
 use proc_macro2::TokenStream as TokenStream2;
@@ -6,19 +12,37 @@ use quote::{ToTokens, TokenStreamExt, quote};
 use super::node::{TranslationNode, TranslationObject};
 use crate::macros::collections::map_transform_to_tokens;
 
-/// Wraps a collection of translation nodes, these translation nodes
-/// are usually directly loaded files, and the keys to access them
-/// independently are the complete system path. The collection
-/// permits searching for translations by iterating all the files
-/// in the specified configuration order, so most likely you don't
-/// need to seek for a translation independently.
+/// Translation file collection.
+///
+/// This tuple struct wraps a hashmap implementing
+/// a lookup trough all the files in ascending order.
+///
+/// The internal hashmap contains the original file
+/// paths along all the unmerged [`TranslationNode`]
+/// found in each file.
 pub struct TranslationNodeCollection(HashMap<String, TranslationNode>);
 
 impl TranslationNodeCollection {
+    /// Create a new [`TranslationNodeCollection`].
+    ///
+    /// By providing a populated hashmap, create a new
+    /// [`TranslationNodeCollection`] structure.
+    ///
+    /// The file paths in the hashmap key aren't validated. This
+    /// is usually called from a `to-runtime` implementation, if
+    /// you want to obtain all the translation files use 
+    ///
+    /// **Arguments**
+    /// * `collection` - An already populated collection for lookup.
+    ///
+    /// **Returns**
+    /// The provided collection wrapped in a [`TranslationNodeCollection`].
     pub fn new(collection: HashMap<String, TranslationNode>) -> Self {
         Self(collection)
     }
 
+    /// Get a node from a file path.
+    ///
     /// This method may be used to load a translation
     /// independently, if you are looking for an independent
     /// translation you may want to call find_path instead.
@@ -35,6 +59,8 @@ impl TranslationNodeCollection {
             .get(path)
     }
 
+    /// Search a path trough all the nodes.
+    ///
     /// This method is used to load a specific translation
     /// file agnostic from a "translation path" which consists
     /// of the necessary TOML object path to reach a specific
@@ -54,17 +80,27 @@ impl TranslationNodeCollection {
     }
 }
 
-/// Abstraction to easily collect a `HashMap` and wrap it
-/// in a `TranslationNodeCollection`.
+/// Hashmap wrapper implementation.
+///
+/// Abstraction to easily collect a [`HashMap<String, TranslationNode>`] and wrap it
+/// in a [`TranslationNodeCollection`].
 impl FromIterator<(String, TranslationNode)> for TranslationNodeCollection {
     fn from_iter<T: IntoIterator<Item = (String, TranslationNode)>>(iter: T) -> Self {
         Self(
-            iter.into_iter()
+            iter
+                .into_iter()
                 .collect(),
         )
     }
 }
 
+/// Compile-time to runtime implementation.
+///
+/// This implementation generates the call to [`new`] on
+/// [`TranslationNodeCollection`] with the data from the current
+/// instance to perform a compile-time to runtime conversion.
+///
+/// [`new`]: TranslationNodeCollection::new
 impl ToTokens for TranslationNodeCollection {
     fn to_tokens(&self, tokens: &mut TokenStream2) {
         let map =
