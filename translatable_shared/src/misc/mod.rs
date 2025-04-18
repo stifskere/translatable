7a6587ff637f8a1f149cbfc82c11b399a78fe063
stifskere <esteve@memw.es>
2025-04-18 02:48:13 +0200

    docs: translatable_shared/src/misc/mod.rs

--- translatable_shared/src/misc/mod.rs
+++ translatable_shared/src/misc/mod.rs
@@ -1,2 +1,8 @@
+//! Uncategorized item module.
+//!
+//! This module contains sub-modules with miscellaneous structures,
+//! or items that don't fit into an existing category — typically
+//! because there aren’t enough related modules to justify their own group.
+
 pub mod language;
 pub mod templating;
